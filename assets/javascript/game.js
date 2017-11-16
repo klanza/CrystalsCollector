b64fd60a75555769245fa541715e985ba97cd70b
klanza <klanza91@gmail.com>
2017-11-16 00:56:07 -0700

    Created jQuery click functions, updated html file backbone

---
+++ assets/javascript/game.js
@@ -0,0 +1,65 @@
+// Variable to store random value to be guessed
+var chosenNumber = "";
+
+// Variables to store random values of crystals
+var crystalOne;
+var crystalTwo;
+var crystalThree;
+var crystalFour;
+
+// Current number as a sum of clicked crystals
+var currentScore = 0
+
+// Stat counter
+var wins = 0
+var losses = 0
+
+
+
+function resetGame() {
+    //Pick random number between 19-120
+    chosenNumber = Math.floor(Math.random()*(120-19 + 1)) + 19;
+
+    //Create random variables tied to each
+    crystalOne = Math.floor(Math.random()*12-1 + 1) + 1;
+    crystalTwo = Math.floor(Math.random()*12-1 + 1) + 1;
+    crystalThree = Math.floor(Math.random()*12-1 + 1) + 1;
+    crystalFour = Math.floor(Math.random()*12-1 + 1) + 1;
+
+    //Reset the score
+    currentScore = 0
+
+    console.log(chosenNumber)
+    console.log(crystalOne)
+    console.log(crystalTwo)
+    console.log(crystalThree)
+    console.log(crystalFour)
+
+}
+
+$("#amethyst").click(function() {
+    currentScore = crystalOne + currentScore
+    console.log(currentScore)
+});
+
+$("#diamond").click(function() {
+    currentScore = crystalTwo + currentScore
+    console.log(currentScore)
+});
+
+$("#emerald").click(function() {
+    currentScore = crystalThree + currentScore
+    console.log(currentScore)
+});
+
+$("#ruby").click(function() {
+    currentScore = crystalFour + currentScore
+    console.log(currentScore)
+});
+
+// function gameButtons() {
+//     $("#amethyst").click(function() {
+//         currentScore += crystalOne
+//         console.log(currentScore)
+//     });
+// }
